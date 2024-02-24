70f85a08a1b3c1475ea9e482bb1f77d20440068e
pradip garala <pradip.garala@yahoo.com>
2024-02-24 10:56:01 +0530

    typo department

--- src/main/java/com/example/webfluxjpa/WebFluxJpaApplication.java
+++ src/main/java/com/example/webfluxjpa/WebFluxJpaApplication.java
@@ -27,7 +27,7 @@ public class WebFluxJpaApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		departmentRepository.save(Department.builder().name("account").description("account department").build());
 		departmentRepository.save(Department.builder().name("engineering").description("engineering department").build());
-		departmentRepository.save(Department.builder().name("ht").description("hr department").build());
+		departmentRepository.save(Department.builder().name("hr").description("hr department").build());
 
 		employeeRepository.save(Employee.builder().name("alice").age(10).email("alice@test.com").departments(Collections.singletonList(departmentRepository.findById(1).get())).build());
 		employeeRepository.save(Employee.builder().name("bob").age(20).email("bob@test.com").departments(Collections.singletonList(departmentRepository.findById(2).get())).build());
