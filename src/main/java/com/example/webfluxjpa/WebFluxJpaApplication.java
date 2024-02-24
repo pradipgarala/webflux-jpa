9b07271b56bef22d302c9c6bdbfef9cb6b26cb65
pradip garala <pradip.garala@yahoo.com>
2024-02-24 19:03:47 +0530

    load data from script

--- src/main/java/com/example/webfluxjpa/WebFluxJpaApplication.java
+++ src/main/java/com/example/webfluxjpa/WebFluxJpaApplication.java
@@ -1,36 +1,12 @@
 package com.example.webfluxjpa;
 
-import com.example.webfluxjpa.entity.Department;
-import com.example.webfluxjpa.entity.Employee;
-import com.example.webfluxjpa.repository.DepartmentRepository;
-import com.example.webfluxjpa.repository.EmployeeRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 @SpringBootApplication
-@RequiredArgsConstructor
-public class WebFluxJpaApplication implements CommandLineRunner {
-
-	private final DepartmentRepository departmentRepository;
-	private final EmployeeRepository employeeRepository;
+public class WebFluxJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebFluxJpaApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		departmentRepository.save(Department.builder().name("account").description("account department").build());
-		departmentRepository.save(Department.builder().name("engineering").description("engineering department").build());
-		departmentRepository.save(Department.builder().name("hr").description("hr department").build());
-
-		employeeRepository.save(Employee.builder().name("alice").age(10).email("alice@test.com").departments(Collections.singletonList(departmentRepository.findById(1).get())).build());
-		employeeRepository.save(Employee.builder().name("bob").age(20).email("bob@test.com").departments(Collections.singletonList(departmentRepository.findById(2).get())).build());
-		employeeRepository.save(Employee.builder().name("charlie").age(20).email("charlie@test.com").departments(departmentRepository.findAllById(Arrays.asList(1, 2))).build());
 	}
 }
--- src/main/java/com/example/webfluxjpa/config/BeanConfig.java
+++ src/main/java/com/example/webfluxjpa/config/BeanConfig.java
@@ -3,14 +3,8 @@ package com.example.webfluxjpa.config;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
-
-import java.util.concurrent.Executors;
 
 @Configuration
 public class BeanConfig {
