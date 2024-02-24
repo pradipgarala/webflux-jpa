d89ace756fde1fcc9007470ebf2597680c461c95
pradip garala <pradip.garala@yahoo.com>
2024-02-24 12:12:51 +0530

    department in POST / PATCH endpoint

--- src/main/java/com/example/webfluxjpa/dto/EmployeeDto.java
+++ src/main/java/com/example/webfluxjpa/dto/EmployeeDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -16,4 +17,5 @@ public class EmployeeDto {
     private Integer age;
     @Email
     private String email;
+    private Set<Integer> departments;
 }
--- src/main/java/com/example/webfluxjpa/service/EmployeeService.java
+++ src/main/java/com/example/webfluxjpa/service/EmployeeService.java
@@ -1,8 +1,10 @@
 package com.example.webfluxjpa.service;
 
 import com.example.webfluxjpa.dto.EmployeeDto;
+import com.example.webfluxjpa.entity.Department;
 import com.example.webfluxjpa.entity.Employee;
 import com.example.webfluxjpa.exception.ResourceNotFoundException;
+import com.example.webfluxjpa.repository.DepartmentRepository;
 import com.example.webfluxjpa.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -11,13 +13,21 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class EmployeeService {
 
     private static final String EMPLOYEE_NOT_FOUND = "Employee not found";
 
     private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
     private final ModelMapper modelMapper;
     private final Scheduler jdbcScheduler;
 
@@ -34,16 +44,35 @@ public class EmployeeService {
 
     public Mono<Employee> save(EmployeeDto dto) {
         Employee entity = modelMapper.map(dto, Employee.class);
+        validateDepartment(dto, entity);
         return save(entity);
     }
+
 
     public Mono<Employee> update(Integer id, EmployeeDto dto) {
         Employee entity = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(EMPLOYEE_NOT_FOUND));
 
         modelMapper.map(dto, entity);
+        validateDepartment(dto, entity);
 
         return save(entity);
+    }
+
+    private void validateDepartment(EmployeeDto dto, Employee entity) {
+        List<Department> departmentList = Optional.ofNullable(dto.getDepartments())
+                .map(departmentRepository::findAllById)
+                .orElseGet(Arrays::asList);
+
+        Integer inputSize = Optional.ofNullable(dto.getDepartments())
+                .map(Set::size)
+                .orElseGet(() -> 0);
+
+        if (inputSize != departmentList.size()) {
+            throw new ResourceNotFoundException("Department(s) not found");
+        }
+
+        entity.setDepartments(departmentList);
     }
 
     private Mono<Employee> save(Employee entity) {
