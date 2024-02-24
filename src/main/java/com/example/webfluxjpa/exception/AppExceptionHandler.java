05af3c7dac5887ee44663b714cff0937d4c35f66
pradip garala <pradip.garala@yahoo.com>
2024-02-24 13:45:40 +0530

    remove unused code

--- src/main/java/com/example/webfluxjpa/exception/AppExceptionHandler.java
+++ src/main/java/com/example/webfluxjpa/exception/AppExceptionHandler.java
@@ -43,26 +43,4 @@ public class AppExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(
-            MethodArgumentNotValidException e) {
-        Map<String, String> errorMap = new HashMap<>();
-
-        e.getBindingResult().getAllErrors()
-                .forEach((error) -> {
-                    String fieldName = ((FieldError) error).getField();
-                    String errorMessage = error.getDefaultMessage();
-
-                    errorMap.put(fieldName, errorMessage);
-                });
-
-
-        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
-    }
-
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
-        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
 }
